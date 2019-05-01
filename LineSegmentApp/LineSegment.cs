38ffd3ee669f90011f144995c0ba7311a024bde7
pkrueger <philip.krueger@caci.com>
2019-05-01 16:39:48 -0700

    first push

---
+++ LineSegmentApp/LineSegment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineSegmentApp
+{
+  class LineSegment
+  {
+    private Point pointOne;
+    private Point pointTwo;
+    private LineSegment(Point p1, Point p2)
+    {
+      pointOne = p1;
+      pointTwo = p2;
+    }
+
+    public LineSegment Create(Point p1, Point p2)
+    {
+      return new LineSegment(p1, p2);
+    }
+  }
+}
---
+++ LineSegmentApp/Point.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace LineSegmentApp
+{
+  class Point
+  {
+    private double x = 0;
+    private double y = 0;
+    private Point(double x, double y)
+    {
+      this.x = x;
+      this.y = y;
+    }
+
+    public static Point create(double x, double y)
+    {
+      return new Point(x, y);
+    }
+
+    public void setPoint(double x, double y)
+    {
+      this.x = x;
+      this.y = y;
+    }
+
+    public double dist(Point p)  // compute the distance of point p to the current point
+    {
+      double distance;
+      distance = Math.Sqrt((x - p.x) * (x - p.x) + (y - p.y) * (y - p.y));
+      return distance;
+    }
+
+    public Point sub(Point p)
+    {
+      return create(x - p.x, y - p.y);
+    }
+
+    public Point norm()
+    {
+      var mag = this.mag();
+      return create(x / mag, y / mag);
+    }
+
+    public double dot(Point p)
+    {
+      return x * p.x + y * p.y;
+    }
+
+    public double mag()
+    {
+      return dist(create(0, 0));
+    }
+
+    public string print()
+    {
+      return $"({x}, {y})";
+    }
+  }
+}
